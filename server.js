fe36c483d0bf521aba158da816f9a08906978ff4
George Stockhammer <sunchipzzz@gmail.com>
2021-11-23 17:51:52 -0600

    added post requests, json file manip, and validations

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express(); //instantiate server
+app.use(express.urlencoded({ extended: true })); // parse incoming string or array data
+app.use(express.json()); // parse incoming JSON data
 const { animals } = require('./data/animals'); //destruct all the animal from the object
 
 app.listen(PORT, ()=>{
@@ -42,6 +46,32 @@ function findById(id, animalsArray){
     return result;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) { //if req.query is not undefined
@@ -54,4 +84,17 @@ app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) res.json(result);
     else res.send(404);
+});
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
 });
